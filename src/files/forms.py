93e46e259f73ca916ac7a42627eab9184884e5a2
vincentrandon <vincentrandon1@gmail.com>
2022-07-13 14:41:42 +0200

    New version

--- src/files/forms.py
+++ src/files/forms.py
@@ -22,7 +22,8 @@ class CompanyFileForm(forms.ModelForm):
         fields = ('file',)
         widgets = {
             'file': forms.FileInput(attrs={'onchange': 'submit();',
-                                           'class': 'ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'}),
+                                           'style': 'display: none;',
+                                           'id': 'company_file'})
         }
 
 
